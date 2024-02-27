1e781de6547c22fb866f7bb41c5ad8cc9d84b54c
Alexander Todorov <atodorov@otb.bg>
2024-02-27 16:45:19 +0200

    Send exceptions to Sentry

---
+++ app/version.py
@@ -0,0 +1,12 @@
+"""
+Version and release date constants!
+
+Copyright (c) 2024 Alexander Todorov <atodorov@otb.bg>
+
+Licensed under the AGPL-3.0: https://www.gnu.org/licenses/agpl-3.0.html
+"""
+
+from datetime import datetime
+
+__version__ = "1"
+__release_date__ = datetime(2024, 2, 27, 13, 5)
