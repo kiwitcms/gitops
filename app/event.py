ff16fc2acbafaf9f7266798d0ddc4b3d7be84d4d
Alexander Todorov <atodorov@otb.bg>
2024-02-29 15:03:00 +0200

    Add .argv & .private properties to GitHubEvent
    and try executing actual cli commands based on comment contents

--- app/event.py
+++ app/event.py
@@ -2,6 +2,7 @@
 Load payload from GitHub, GitLab, Azure DevOps, etc
 """
 
+# pylint: disable=missing-function-docstring
 #
 # Copyright (c) 2024 Alexander Todorov <atodorov@otb.bg>
 #
@@ -27,6 +28,7 @@ def load():
 
 
 class TriggerEvent:
+    argv = []
     private = True
 
     def __init__(self, file_path):
@@ -35,6 +37,8 @@ class TriggerEvent:
 
 
 class GitHubEvent(TriggerEvent):
+    comment = None
+
     def __init__(self, file_path):
         super().__init__(file_path)
 
@@ -42,13 +46,25 @@ class GitHubEvent(TriggerEvent):
             auth=github.Auth.Token(os.environ["INPUT_TOKEN"]),
             base_url=os.environ["GITHUB_API_URL"],
         )
-        self.comment = github.IssueComment.IssueComment(
-            self.api._Github__requester, {}, self.payload["comment"], completed=True
-        )
-
-        self.private = self.payload["repository"]["private"]
 
         if self.private:
             raise RuntimeError(
                 "See https://kiwitcms.org/#subscriptions for running against private repositories!"
             )
+
+    @property
+    def private(self):
+        return self.payload["repository"]["private"]
+
+    @property
+    def argv(self):
+        if "comment" in self.payload:
+            self.comment = github.IssueComment.IssueComment(
+                self.api._Github__requester,  # pylint: disable=protected-access
+                {},
+                self.payload["comment"],
+                completed=True,
+            )
+            return self.comment.body.strip().split()
+
+        raise RuntimeError("unrecognized command argument")
