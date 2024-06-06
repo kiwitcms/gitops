79013c2dd0ad567f30ac2d28f0612ec49de62164
atodorov <atodorov@users.noreply.github.com>
2024-06-06 11:46:52 +0000

    [MegaLinter] Apply linters fixes

--- app/analytics.py
+++ app/analytics.py
@@ -8,10 +8,9 @@ Anonymous analytics via Plausible.io
 # Licensed under the AGPL-3.0: https://www.gnu.org/licenses/agpl-3.0.html
 #
 
-import os
 import math
+import os
 import time
-
 from functools import wraps
 from hashlib import sha256
 from http import HTTPStatus
